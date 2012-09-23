fbead80bdd5e7220297cf78b2193df5eb4372002
Jekyll Wu <adaptee@gmail.com>
2012-09-23 21:40:00 +0800

    Non void function should return something
    This fixes building with clang

--- libmenu-cache/menu-cache.c
+++ libmenu-cache/menu-cache.c
@@ -226,7 +226,7 @@ static MenuCacheItem* read_item(  FILE* f, MenuCache* cache )
 
     /* file name */
     if( G_UNLIKELY( ! fgets( line, G_N_ELEMENTS(line) - 1, f ) ))
-        return;
+        return NULL;
     len = strlen( line );
     if( G_LIKELY(len > 1) )
         item->file_name = g_strndup( line, len - 1 );
@@ -240,7 +240,7 @@ static MenuCacheItem* read_item(  FILE* f, MenuCache* cache )
 
     /* desktop file dir */
     if( G_UNLIKELY( ! fgets( line, G_N_ELEMENTS(line) - 1, f ) ))
-        return;
+        return NULL;
     idx = atoi( line );
     if( G_LIKELY( idx >=0 && idx < cache->n_all_used_files ) )
         item->file_dir = cache->all_used_files[ idx ] + 1;
