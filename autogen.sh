958ef8a53ca133b57573b8533f9a8332ec52a434
Veeti Paananen <veeti.paananen@rojekti.fi>
2013-11-04 04:00:10 +0200

    Configure by default

--- autogen.sh
+++ autogen.sh
@@ -44,4 +44,9 @@ $AUTOMAKE --add-missing --copy --include-deps
 ${AUTOCONF:-autoconf$AC_VERSION}
 
 rm -rf autom4te.cache
+
+if test "x$NOCONFIGURE" = "x"; then
+    ./configure $@
+fi
+
 cd "$OLD_DIR"
