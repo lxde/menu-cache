30ca9ebf2aff4487532bd0460a80ac6bffe00875
Andriy Grytsenko <andrej@rep.kiev.ua>
2014-08-14 20:29:45 +0300

    Fix crash on separator item (after last commit).

--- libmenu-cache/menu-cache.c
+++ libmenu-cache/menu-cache.c
@@ -340,7 +340,7 @@ _fail:
 
 static void menu_cache_file_dir_unref(MenuCacheFileDir *file_dir)
 {
-    if (g_atomic_int_dec_and_test(&file_dir->n_ref))
+    if (file_dir && g_atomic_int_dec_and_test(&file_dir->n_ref))
     {
         g_free(file_dir->dir);
         g_free(file_dir);
