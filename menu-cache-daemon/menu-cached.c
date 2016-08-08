5800ac60392bb1dce41e6aa08a07cfbbd333b16b
Mamoru TASAKA <mtasaka@fedoraproject.org>
2016-08-08 21:01:50 +0900

    Call g_source_remove when destoying channel
    ref: https://github.com/lxde/menu-cache/issues/7
    
    It seems that even if the client side of *socket* is already
    closed, the server side cannot receive POLLHUP. And when
    it already happened, at the time the server tries to write
    into the socket, SIGPIPE is received, then we firstly know that
    the client is already closed.
    
    Also, then closing GIOChannel, we must explicitly remove the
    "source" added by g_io_add_watch, otherwise we see 100 percent
    CPU usage by poll.
    
    So we must register source id as well as channel simultaneously,
    and call shutdown function explicitly when we receive
    SIGPIPE.

--- menu-cache-daemon/menu-cached.c
+++ menu-cache-daemon/menu-cached.c
@@ -76,6 +76,11 @@ typedef struct _Cache
     char *cache_file;
 }Cache;
 
+typedef struct ClientIO_ {
+    guint source_id;
+    GIOChannel *channel;
+} ClientIO;
+
 static GMainLoop* main_loop = NULL;
 
 static GHashTable* hash = NULL;
@@ -84,6 +89,8 @@ static char *socket_file = NULL;
 
 static void on_file_changed( GFileMonitor* mon, GFile* gf, GFile* other,
                              GFileMonitorEvent evt, Cache* cache );
+
+static void on_client_closed(gpointer user_data);
 
 static gboolean delayed_cache_free(gpointer data)
 {
@@ -317,9 +324,12 @@ static void do_reload(Cache* cache)
     /* notify the clients that reload is needed. */
     for( l = cache->clients; l; l = l->next )
     {
-        GIOChannel* ch = (GIOChannel*)l->data;
+        ClientIO *channel_io = (ClientIO *)l->data;
+        GIOChannel* ch = channel_io->channel;
         if(write(g_io_channel_unix_get_fd(ch), buf, 37) < 37)
-            g_io_channel_shutdown(ch, FALSE, NULL);
+        {
+            on_client_closed(channel_io);
+        }
     }
     cache->need_reload = FALSE;
 }
@@ -503,16 +513,18 @@ static int create_socket(struct sockaddr_un *addr)
 
 static void on_client_closed(gpointer user_data)
 {
-    GIOChannel* ch = user_data;
+    ClientIO* client_io = user_data;
     GHashTableIter it;
     char* md5;
     Cache* cache;
     GSList *l;
+    GIOChannel *ch = client_io->channel;
+
     DEBUG("client closed: %p", ch);
     g_hash_table_iter_init (&it, hash);
     while( g_hash_table_iter_next (&it, (gpointer*)&md5, (gpointer*)&cache) )
     {
-        while((l = g_slist_find( cache->clients, ch )) != NULL)
+        while((l = g_slist_find( cache->clients, client_io )) != NULL)
         {
             /* FIXME: some clients are closed accidentally without
              * unregister the menu first due to crashes.
@@ -530,6 +542,9 @@ static void on_client_closed(gpointer user_data)
         }
     }
     /* DEBUG("client closed"); */
+
+    g_source_remove(client_io->source_id);
+    g_free(client_io);
 }
 
 static gboolean on_client_data_in(GIOChannel* ch, GIOCondition cond, gpointer user_data)
@@ -544,6 +559,7 @@ static gboolean on_client_data_in(GIOChannel* ch, GIOCondition cond, gpointer us
 
     if(cond & (G_IO_HUP|G_IO_ERR) )
     {
+        on_client_closed(user_data);
         return FALSE; /* remove the watch */
     }
 
@@ -551,8 +567,10 @@ retry:
     st = g_io_channel_read_line( ch, &line, &len, NULL, NULL );
     if( st == G_IO_STATUS_AGAIN )
         goto retry;
-    if( st != G_IO_STATUS_NORMAL )
+    if( st != G_IO_STATUS_NORMAL ) {
+        on_client_closed(user_data);
         return FALSE;
+    }
 
     --len;
     line[len] = 0;
@@ -659,7 +677,7 @@ retry:
             }
         }
         /* DEBUG("menu %s requested by client %d", md5, g_io_channel_unix_get_fd(ch)); */
-        cache->clients = g_slist_prepend(cache->clients, ch);
+        cache->clients = g_slist_prepend(cache->clients, user_data);
         if(cache->delayed_free_handler)
         {
             g_source_remove(cache->delayed_free_handler);
@@ -685,7 +703,7 @@ retry:
         if(cache && cache->clients)
         {
             /* remove the IO channel from the cache */
-            cache->clients = g_slist_remove(cache->clients, ch);
+            cache->clients = g_slist_remove(cache->clients, user_data);
             if(cache->clients == NULL)
                 cache_free(cache);
         }
@@ -703,6 +721,7 @@ static gboolean on_new_conn_incoming(GIOChannel* ch, GIOCondition cond, gpointer
     socklen_t client_addrlen;
     struct sockaddr client_addr;
     GIOChannel* child;
+    ClientIO* client_io;
 
     server = g_io_channel_unix_get_fd(ch);
 
@@ -716,8 +735,12 @@ static gboolean on_new_conn_incoming(GIOChannel* ch, GIOCondition cond, gpointer
 
     child = g_io_channel_unix_new(client);
     g_io_channel_set_close_on_unref( child, TRUE );
-    g_io_add_watch_full(child, G_PRIORITY_DEFAULT, G_IO_PRI|G_IO_IN|G_IO_HUP|G_IO_ERR,
-                        on_client_data_in, child, on_client_closed);
+
+    client_io = g_new0 (ClientIO, 1);
+    client_io->channel = child;
+    client_io->source_id =
+        g_io_add_watch(child, G_IO_PRI|G_IO_IN|G_IO_HUP|G_IO_ERR,
+                        on_client_data_in, client_io);
     g_io_channel_unref(child);
 
     DEBUG("new client accepted: %p", child);
@@ -823,7 +846,6 @@ int main(int argc, char** argv)
     ch = g_io_channel_unix_new(server_fd);
     if(!ch)
         return 1;
-    g_io_channel_set_close_on_unref(ch, TRUE);
     g_io_add_watch(ch, G_IO_IN|G_IO_PRI, on_new_conn_incoming, NULL);
     g_io_add_watch(ch, G_IO_ERR, on_server_conn_close, NULL);
     g_io_channel_unref(ch);
