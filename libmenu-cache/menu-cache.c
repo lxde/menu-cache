56f66684592abf257c4004e6e1fff041c64a12ce
Andriy Grytsenko <andrej@rep.kiev.ua>
2017-05-14 21:28:00 +0300

    Fix potential access violation, use runtime user dir instead of tmp dir.
    Note: it limits libmenu-cache compatibility to menu-cached >= 0.7.0.

--- libmenu-cache/menu-cache.c
+++ libmenu-cache/menu-cache.c
@@ -3,7 +3,7 @@
  *
  *      Copyright 2008 PCMan <pcman.tw@gmail.com>
  *      Copyright 2009 Jürgen Hötzel <juergen@archlinux.org>
- *      Copyright 2012-2015 Andriy Grytsenko (LStranger) <andrej@rep.kiev.ua>
+ *      Copyright 2012-2017 Andriy Grytsenko (LStranger) <andrej@rep.kiev.ua>
  *
  *      This library is free software; you can redistribute it and/or
  *      modify it under the terms of the GNU Lesser General Public
@@ -1538,8 +1538,13 @@ static void get_socket_name( char* buf, int len )
         if(*p)
             *p = '\0';
     }
+#if GLIB_CHECK_VERSION(2, 28, 0)
+    g_snprintf( buf, len, "%s/menu-cached-%s", g_get_user_runtime_dir(),
+                dpy ? dpy : ":0" );
+#else
     g_snprintf( buf, len, "%s/.menu-cached-%s-%s", g_get_tmp_dir(),
                 dpy ? dpy : ":0", g_get_user_name() );
+#endif
     g_free(dpy);
 }
 
--- menu-cache-daemon/menu-cached.c
+++ menu-cache-daemon/menu-cached.c
@@ -473,6 +473,9 @@ static void get_socket_name( char* buf, int len )
         if(*p)
             *p = '\0';
     }
+    /* NOTE: this socket name is incompatible with versions > 1.0.2,
+            although this function is never used since 0.7.0 but
+            libmenu-cache always requests exact socket name instead */
     g_snprintf( buf, len, "%s/.menu-cached-%s-%s", g_get_tmp_dir(),
                 dpy ? dpy : ":0", g_get_user_name() );
     g_free(dpy);
