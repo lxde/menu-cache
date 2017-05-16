lxde/menu-cache

commit 67a5484b41709f955b1694470f2b79c057a1898e

move daemonizing code to menu-cached.c

menu-cache-daemon/menu-cached.c
@@ -33,6 +33,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/fcntl.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <signal.h>
@@ -489,7 +490,6 @@ static int create_socket()
     addr.sun_family = AF_UNIX;
 
     get_socket_name( addr.sun_path, sizeof( addr.sun_path ) );
-
     if(bind(fd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
     {
         g_debug("Failed to bind to socket");
@@ -706,12 +706,49 @@ int main(int argc, char** argv)
 {
     GMainLoop* main_loop = g_main_loop_new( NULL, TRUE );
     GIOChannel* ch;
-    int fd;
+    int fd, pid;
 
     fd = create_socket();
+
     if( fd < 0 )
         return 1;
 
+    /* Become a daemon */
+    if (pid == 0)
+        {
+            int fd;
+            long open_max;
+            long i;
+            
+            /* don't hold open fd opened besides server socket */
+            open_max = sysconf (_SC_OPEN_MAX);
+            for (i = 0; i < open_max; i++) 
+                {
+                    if (i != fd)
+                        fcntl (i, F_SETFD, FD_CLOEXEC);
+                }
+            
+            /* /dev/null for stdin, stdout, stderr */
+            fd = open ("/dev/null", O_RDONLY);
+            if (fd != -1)
+                {
+                    dup2 (fd, 0);
+                    close (fd);
+                }
+            fd = open ("/dev/null", O_WRONLY);
+            if (fd != -1)
+                {
+                    dup2 (fd, 1);
+                    dup2 (fd, 2);
+                    close (fd);
+                }
+            setsid();
+            /* parent process terminates */
+            if (fork() != 0) 	
+                exit(0);
+        }
+
+    /* child continues */
     signal(SIGHUP, terminate);
     signal(SIGINT, terminate);
     signal(SIGQUIT, terminate);

libmenu-cache/menu-cache.c
@@ -679,40 +679,12 @@ static gboolean fork_server()
         g_print("failed to find menu-cached\n");
     }
 
-    /* Become a daemon */
+    /* Start daemon */
     pid = fork();
     if (pid == 0)
     {
-        int fd;
-        long open_max;
-        long i;
-
-        /* don't hold open fd opened from the client of the library */
-        open_max = sysconf (_SC_OPEN_MAX);
-        for (i = 0; i < open_max; i++)
-            fcntl (i, F_SETFD, FD_CLOEXEC);
-
-        /* /dev/null for stdin, stdout, stderr */
-        fd = open ("/dev/null", O_RDONLY);
-        if (fd != -1)
-        {
-            dup2 (fd, 0);
-            close (fd);
-        }
-        fd = open ("/dev/null", O_WRONLY);
-        if (fd != -1)
-        {
-            dup2 (fd, 1);
-            dup2 (fd, 2);
-            close (fd);
-        }
-        setsid();
-        if (fork() == 0)
-        {
-            execl( server_path, server_path, NULL);
-            g_print("failed to exec %s\n", server_path);
-        }
-        _exit(0);
+        execl( server_path, server_path, NULL);
+        g_print("failed to exec %s\n", server_path);
     }
 
     /*

commit 1586d73c83c043d28259a1b2f28541a39d163fff

Fix daemonization
Close all inherited fds, not just mark them to-be-closed in spawned process.
Regression by 67a5484b41709f955b1694470f2b79c057a1898e (when this code
was executed inside library, exec(daemon) implictly closed all marked fds; in
daemon, fds should be explicitly closed).

menu-cache-daemon/menu-cached.c
@@ -818,12 +818,12 @@ int main(int argc, char** argv)
         g_error("can't change directory to /");
     }
 
+    /* don't hold open fd opened besides server socket and std{in,out,err} */
     open_max = sysconf (_SC_OPEN_MAX);
-    for (i = 0; i < open_max; i++)
+    for (i = 3; i < open_max; i++)
     {
-        /* don't hold open fd opened besides server socket */
         if (i != server_fd)
-            fcntl (i, F_SETFD, FD_CLOEXEC);
+            close (i);
     }
 
     /* /dev/null for stdin, stdout, stderr */