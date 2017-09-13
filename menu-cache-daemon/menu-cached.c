lxde/menu-cache

commit c8794da0cf5a194b5eaadf020f02a5cab89f54fc

Add menu-cache-daemon to monitor file changes. Implement basic IPC between menu-cache lib and the daemon with socket.

menu-cache-daemon/menu-cached.c
@@ -1,0 +1,223 @@
+/*
+ *      menu-cached.c
+ *      
+ *      Copyright 2008 PCMan <pcman@thinkpad>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <gio/gio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+static void get_socket_name( char* buf, int len )
+{
+    char* dpy = g_getenv("DISPLAY");
+    g_snprintf( buf, len, "/tmp/.menu-cached-%s-%s", dpy, g_get_user_name() );
+}
+
+static int create_socket()
+{
+    int fd = -1;
+    struct sockaddr_un addr;
+
+    fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0)
+    {
+        g_print("Failed to create socket\n");
+        return FALSE;
+    }
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+
+    get_socket_name( addr.sun_path, sizeof( addr.sun_path ) );
+
+    if(bind(fd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+    {
+        g_debug("Failed to bind to socket");
+        close(fd);
+        return 1;
+    }
+    if(listen(fd, 30) < 0)
+    {
+        g_debug( "Failed to listen to socket" );
+        close(fd);
+        return 1;
+    }
+    return fd;
+}
+
+static char* set_env( char* penv, const char* name )
+{
+    char* sep = strchr(penv, '\t');
+    if( G_LIKELY(sep) )
+        *sep = '\0';
+    if( *penv )
+        g_setenv( name, penv, TRUE);
+    else
+        g_unsetenv( name );
+    return (sep + 1);
+}
+
+static void pre_exec( gpointer user_data )
+{
+    char* env = (char*)user_data;
+    env = set_env(env, "XDG_CONFIG_DIRS");
+    env = set_env(env, "XDG_MENU_PREFIX");
+    env = set_env(env, "XDG_DATA_DIRS");
+    env = set_env(env, "XDG_CONFIG_HOME");
+    set_env(env, "XDG_DATA_HOME");
+}
+
+static gboolean on_client_in(GIOChannel* ch, GIOCondition cond, gpointer user_data)
+{
+    char *line;
+    gsize len;
+    GIOStatus st;
+    const char* md5;
+
+    g_debug("child data");
+retry:
+    st = g_io_channel_read_line( ch, &line, &len, NULL, NULL );
+    if( st == G_IO_STATUS_AGAIN )
+        goto retry;
+    if( st != G_IO_STATUS_NORMAL )
+        return FALSE;
+
+    --len;
+    line[len] = 0;
+
+    if( memcmp(line, "REG:", 4) == 0 )
+    {
+        GChecksum *sum;
+        int status = 0;
+        char *argv[] = {"/usr/bin/menu-cache-gen", "-l", NULL, "-i", NULL, "-o", NULL, NULL};
+        char *pline = line + 4;
+        char *sep, *menu_name, *lang_name;
+        len -= 4;
+
+        /* Format of received string, separated by '\t'.
+         * Menu Name
+         * Language Name
+         * XDG_CONFIG_DIRS
+         * XDG_MENU_PREFIX
+         * XDG_DATA_DIRS
+         * XDG_CONFIG_HOME
+         * XDG_DATA_HOME */
+        sum = g_checksum_new(G_CHECKSUM_MD5);
+        g_checksum_update(sum, pline, len);
+        md5 = g_checksum_get_string(sum);
+
+        sep = strchr(pline, '\t');
+        *sep = '\0';
+        menu_name = pline;
+        pline = sep + 1;
+
+        sep = strchr(pline, '\t');
+        *sep = '\0';
+        lang_name = pline;
+        pline = sep + 1;
+
+        argv[2] = lang_name;
+        argv[4] = menu_name;
+        /* FIXME: should obtain cache dir from client's env */
+        argv[6] = g_build_filename(g_get_user_cache_dir(), "menus", md5, NULL );
+
+        if( !g_spawn_sync(NULL, argv, NULL, G_SPAWN_SEARCH_PATH,
+                        pre_exec, pline, NULL, NULL, &status, NULL ))
+        {
+            g_debug("error executing menu-cache-gen");
+        }
+        g_free( argv[6] );
+
+        if( status  == 0 )
+            write( g_io_channel_unix_get_fd(ch), md5, 32 );
+        else
+            write( g_io_channel_unix_get_fd(ch), "", 0 );
+        g_checksum_free( sum );
+    }
+    else if( memcmp(line, "UNR:", 4) == 0 )
+    {
+        md5 = line + 4;
+        g_debug("unregister: %s", md5);
+    }
+    g_free( line );
+
+    return TRUE;
+}
+
+static gboolean on_client_closed(GIOChannel* ch, GIOCondition cond, gpointer user_data)
+{
+    g_debug("client closed");
+    return FALSE;
+}
+
+static gboolean on_conn_in(GIOChannel* ch, GIOCondition cond, gpointer user_data)
+{
+    int server, client;
+    socklen_t client_addrlen;
+    struct sockaddr client_addr;
+    GIOChannel* child;
+
+    server = g_io_channel_unix_get_fd(ch);
+
+    client_addrlen = sizeof(client_addr);
+    client = accept(server, &client_addr, &client_addrlen);
+    if( client == -1 )
+    {
+        g_debug("accept error");
+        return TRUE;
+    }
+
+    child = g_io_channel_unix_new(client);
+    g_io_add_watch( child, G_IO_PRI|G_IO_IN, on_client_in, NULL );
+    g_io_add_watch( child, G_IO_HUP|G_IO_ERR, on_client_closed, NULL );
+    g_io_channel_set_close_on_unref( child, TRUE );
+
+    return TRUE;
+}
+
+static gboolean on_conn_close(GIOChannel* ch, GIOCondition cond, gpointer user_data)
+{
+    return TRUE;
+}
+
+int main(int argc, char** argv)
+{
+    GMainLoop* main_loop = g_main_loop_new( NULL, TRUE );
+    GIOChannel* ch;
+    int fd = create_socket();
+    ch = g_io_channel_unix_new(fd);
+    if(!ch)
+        return 1;
+    g_io_channel_set_close_on_unref(ch, TRUE);
+    g_io_add_watch(ch, G_IO_IN|G_IO_PRI, on_conn_in, NULL);
+    g_io_add_watch(ch, G_IO_ERR, on_conn_close, NULL);
+
+    g_main_loop_run( main_loop );
+    g_main_loop_unref( main_loop );
+
+    {
+        char path[256];
+        get_socket_name(path, 256 );
+        unlink(path);
+    }
+    return 0;
+}

Makefile.am
@@ -4,6 +4,7 @@ ALL_SUBDIRS = 			\
 	po 			\
 	libmenu-cache		\
 	menu-cache-gen		\
+	menu-cache-daemon	\
 	$(NULL)
 
 SUBDIRS = $(ALL_SUBDIRS)

configure.in
@@ -59,5 +59,6 @@ libmenu-cache/Makefile
 libmenu-cache/libmenu-cache.pc
 libmenu-cache/libmenu-cache-uninstalled.pc
 menu-cache-gen/Makefile
+menu-cache-daemon/Makefile
 po/Makefile.in
 ])

libmenu-cache/Makefile.in
@@ -16,7 +16,6 @@
 
 
 
-
 VPATH = @srcdir@
 pkgdatadir = $(datadir)/@PACKAGE@
 pkglibdir = $(libdir)/@PACKAGE@
@@ -35,7 +34,6 @@ PRE_UNINSTALL = :
 POST_UNINSTALL = :
 build_triplet = @build@
 host_triplet = @host@
-bin_PROGRAMS = test_menu$(EXEEXT)
 subdir = libmenu-cache
 DIST_COMMON = $(lib_menu_cache_include_HEADERS) $(srcdir)/Makefile.am \
 	$(srcdir)/Makefile.in \
@@ -54,8 +52,7 @@ am__vpath_adj = case $$p in \
     *) f=$$p;; \
   esac;
 am__strip_dir = `echo $$p | sed -e 's|^.*/||'`;
-am__installdirs = "$(DESTDIR)$(libdir)" "$(DESTDIR)$(bindir)" \
-	"$(DESTDIR)$(pkgconfigdir)" \
+am__installdirs = "$(DESTDIR)$(libdir)" "$(DESTDIR)$(pkgconfigdir)" \
 	"$(DESTDIR)$(lib_menu_cache_includedir)"
 libLTLIBRARIES_INSTALL = $(INSTALL)
 LTLIBRARIES = $(lib_LTLIBRARIES)
@@ -68,14 +65,6 @@ libmenu_cache_la_OBJECTS = $(am_libmenu_cache_la_OBJECTS)
 libmenu_cache_la_LINK = $(LIBTOOL) --tag=CC $(AM_LIBTOOLFLAGS) \
 	$(LIBTOOLFLAGS) --mode=link $(CCLD) $(AM_CFLAGS) $(CFLAGS) \
 	$(libmenu_cache_la_LDFLAGS) $(LDFLAGS) -o $@
-binPROGRAMS_INSTALL = $(INSTALL_PROGRAM)
-PROGRAMS = $(bin_PROGRAMS)
-am_test_menu_OBJECTS = test_menu-test.$(OBJEXT)
-test_menu_OBJECTS = $(am_test_menu_OBJECTS)
-test_menu_DEPENDENCIES = $(am__DEPENDENCIES_1) libmenu-cache.la
-test_menu_LINK = $(LIBTOOL) --tag=CC $(AM_LIBTOOLFLAGS) \
-	$(LIBTOOLFLAGS) --mode=link $(CCLD) $(test_menu_CFLAGS) \
-	$(CFLAGS) $(AM_LDFLAGS) $(LDFLAGS) -o $@
 DEFAULT_INCLUDES = -I.@am__isrc@ -I$(top_builddir)
 depcomp = $(SHELL) $(top_srcdir)/depcomp
 am__depfiles_maybe = depfiles
@@ -88,8 +77,8 @@ CCLD = $(CC)
 LINK = $(LIBTOOL) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) \
 	--mode=link $(CCLD) $(AM_CFLAGS) $(CFLAGS) $(AM_LDFLAGS) \
 	$(LDFLAGS) -o $@
-SOURCES = $(libmenu_cache_la_SOURCES) $(test_menu_SOURCES)
-DIST_SOURCES = $(libmenu_cache_la_SOURCES) $(test_menu_SOURCES)
+SOURCES = $(libmenu_cache_la_SOURCES)
+DIST_SOURCES = $(libmenu_cache_la_SOURCES)
 pkgconfigDATA_INSTALL = $(INSTALL_DATA)
 DATA = $(pkgconfig_DATA)
 lib_menu_cache_includeHEADERS_INSTALL = $(INSTALL_HEADER)
@@ -286,11 +275,6 @@ EXTRA_DIST = \
         libmenu-cache-uninstalled.pc.in	\
 	$(NULL)
 
-test_menu_SOURCES = test.c
-test_menu_CFLAGS = $(GLIB_CFLAGS) -I/usr/include/gtk-2.0 -I/usr/lib/gtk-2.0/include -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/freetype2 -I/usr/include/libpng12 
-test_menu_LDADD = $(GLIB_LIBS) libmenu-cache.la \
--lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 -lpangoft2-1.0 -lgdk_pixbuf-2.0 -lm -lpangocairo-1.0 -lgio-2.0 -lcairo -lpango-1.0 -lfreetype -lz -lfontconfig -lgobject-2.0 -lgmodule-2.0 -lglib-2.0 
-
 pkgconfigdir = $(libdir)/pkgconfig
 pkgconfig_DATA = libmenu-cache.pc
 all: all-am
@@ -359,37 +343,6 @@ clean-libLTLIBRARIES:
 	done
 libmenu-cache.la: $(libmenu_cache_la_OBJECTS) $(libmenu_cache_la_DEPENDENCIES) 
 	$(libmenu_cache_la_LINK) -rpath $(libdir) $(libmenu_cache_la_OBJECTS) $(libmenu_cache_la_LIBADD) $(LIBS)
-install-binPROGRAMS: $(bin_PROGRAMS)
-	@$(NORMAL_INSTALL)
-	test -z "$(bindir)" || $(MKDIR_P) "$(DESTDIR)$(bindir)"
-	@list='$(bin_PROGRAMS)'; for p in $$list; do \
-	  p1=`echo $$p|sed 's/$(EXEEXT)$$//'`; \
-	  if test -f $$p \
-	     || test -f $$p1 \
-	  ; then \
-	    f=`echo "$$p1" | sed 's,^.*/,,;$(transform);s/$$/$(EXEEXT)/'`; \
-	   echo " $(INSTALL_PROGRAM_ENV) $(LIBTOOL) $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=install $(binPROGRAMS_INSTALL) '$$p' '$(DESTDIR)$(bindir)/$$f'"; \
-	   $(INSTALL_PROGRAM_ENV) $(LIBTOOL) $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=install $(binPROGRAMS_INSTALL) "$$p" "$(DESTDIR)$(bindir)/$$f" || exit 1; \
-	  else :; fi; \
-	done
-
-uninstall-binPROGRAMS:
-	@$(NORMAL_UNINSTALL)
-	@list='$(bin_PROGRAMS)'; for p in $$list; do \
-	  f=`echo "$$p" | sed 's,^.*/,,;s/$(EXEEXT)$$//;$(transform);s/$$/$(EXEEXT)/'`; \
-	  echo " rm -f '$(DESTDIR)$(bindir)/$$f'"; \
-	  rm -f "$(DESTDIR)$(bindir)/$$f"; \
-	done
-
-clean-binPROGRAMS:
-	@list='$(bin_PROGRAMS)'; for p in $$list; do \
-	  f=`echo $$p|sed 's/$(EXEEXT)$$//'`; \
-	  echo " rm -f $$p $$f"; \
-	  rm -f $$p $$f ; \
-	done
-test_menu$(EXEEXT): $(test_menu_OBJECTS) $(test_menu_DEPENDENCIES) 
-	@rm -f test_menu$(EXEEXT)
-	$(test_menu_LINK) $(test_menu_OBJECTS) $(test_menu_LDADD) $(LIBS)
 
 mostlyclean-compile:
 	-rm -f *.$(OBJEXT)
@@ -398,7 +351,6 @@ distclean-compile:
 	-rm -f *.tab.c
 
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/menu-cache.Plo@am__quote@
-@AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/test_menu-test.Po@am__quote@
 
 .c.o:
 @am__fastdepCC_TRUE@	$(COMPILE) -MT $@ -MD -MP -MF $(DEPDIR)/$*.Tpo -c -o $@ $<
@@ -420,20 +372,6 @@ distclean-compile:
 @AMDEP_TRUE@@am__fastdepCC_FALSE@	source='$<' object='$@' libtool=yes @AMDEPBACKSLASH@
 @AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
 @am__fastdepCC_FALSE@	$(LTCOMPILE) -c -o $@ $<
-
-test_menu-test.o: test.c
-@am__fastdepCC_TRUE@	$(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(test_menu_CFLAGS) $(CFLAGS) -MT test_menu-test.o -MD -MP -MF $(DEPDIR)/test_menu-test.Tpo -c -o test_menu-test.o `test -f 'test.c' || echo '$(srcdir)/'`test.c
-@am__fastdepCC_TRUE@	mv -f $(DEPDIR)/test_menu-test.Tpo $(DEPDIR)/test_menu-test.Po
-@AMDEP_TRUE@@am__fastdepCC_FALSE@	source='test.c' object='test_menu-test.o' libtool=no @AMDEPBACKSLASH@
-@AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
-@am__fastdepCC_FALSE@	$(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(test_menu_CFLAGS) $(CFLAGS) -c -o test_menu-test.o `test -f 'test.c' || echo '$(srcdir)/'`test.c
-
-test_menu-test.obj: test.c
-@am__fastdepCC_TRUE@	$(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(test_menu_CFLAGS) $(CFLAGS) -MT test_menu-test.obj -MD -MP -MF $(DEPDIR)/test_menu-test.Tpo -c -o test_menu-test.obj `if test -f 'test.c'; then $(CYGPATH_W) 'test.c'; else $(CYGPATH_W) '$(srcdir)/test.c'; fi`
-@am__fastdepCC_TRUE@	mv -f $(DEPDIR)/test_menu-test.Tpo $(DEPDIR)/test_menu-test.Po
-@AMDEP_TRUE@@am__fastdepCC_FALSE@	source='test.c' object='test_menu-test.obj' libtool=no @AMDEPBACKSLASH@
-@AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
-@am__fastdepCC_FALSE@	$(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(test_menu_CFLAGS) $(CFLAGS) -c -o test_menu-test.obj `if test -f 'test.c'; then $(CYGPATH_W) 'test.c'; else $(CYGPATH_W) '$(srcdir)/test.c'; fi`
 
 mostlyclean-libtool:
 	-rm -f *.lo
@@ -550,11 +488,9 @@ distdir: $(DISTFILES)
 	done
 check-am: all-am
 check: check-am
-all-am: Makefile $(LTLIBRARIES) $(PROGRAMS) $(DATA) $(HEADERS)
-install-binPROGRAMS: install-libLTLIBRARIES
-
+all-am: Makefile $(LTLIBRARIES) $(DATA) $(HEADERS)
 installdirs:
-	for dir in "$(DESTDIR)$(libdir)" "$(DESTDIR)$(bindir)" "$(DESTDIR)$(pkgconfigdir)" "$(DESTDIR)$(lib_menu_cache_includedir)"; do \
+	for dir in "$(DESTDIR)$(libdir)" "$(DESTDIR)$(pkgconfigdir)" "$(DESTDIR)$(lib_menu_cache_includedir)"; do \
 	  test -z "$$dir" || $(MKDIR_P) "$$dir"; \
 	done
 install: install-am
@@ -583,8 +519,8 @@ maintainer-clean-generic:
 	@echo "it deletes files that may require special tools to rebuild."
 clean: clean-am
 
-clean-am: clean-binPROGRAMS clean-generic clean-libLTLIBRARIES \
-	clean-libtool mostlyclean-am
+clean-am: clean-generic clean-libLTLIBRARIES clean-libtool \
+	mostlyclean-am
 
 distclean: distclean-am
 	-rm -rf ./$(DEPDIR)
@@ -607,7 +543,7 @@ install-data-am: install-lib_menu_cache_includeHEADERS \
 
 install-dvi: install-dvi-am
 
-install-exec-am: install-binPROGRAMS install-libLTLIBRARIES
+install-exec-am: install-libLTLIBRARIES
 
 install-html: install-html-am
 
@@ -639,27 +575,26 @@ ps: ps-am
 
 ps-am:
 
-uninstall-am: uninstall-binPROGRAMS uninstall-libLTLIBRARIES \
+uninstall-am: uninstall-libLTLIBRARIES \
 	uninstall-lib_menu_cache_includeHEADERS \
 	uninstall-pkgconfigDATA
 
 .MAKE: install-am install-strip
 
-.PHONY: CTAGS GTAGS all all-am check check-am clean clean-binPROGRAMS \
-	clean-generic clean-libLTLIBRARIES clean-libtool ctags \
-	distclean distclean-compile distclean-generic \
-	distclean-libtool distclean-tags distdir dvi dvi-am html \
-	html-am info info-am install install-am install-binPROGRAMS \
-	install-data install-data-am install-dvi install-dvi-am \
-	install-exec install-exec-am install-html install-html-am \
-	install-info install-info-am install-libLTLIBRARIES \
-	install-lib_menu_cache_includeHEADERS install-man install-pdf \
-	install-pdf-am install-pkgconfigDATA install-ps install-ps-am \
-	install-strip installcheck installcheck-am installdirs \
-	maintainer-clean maintainer-clean-generic mostlyclean \
-	mostlyclean-compile mostlyclean-generic mostlyclean-libtool \
-	pdf pdf-am ps ps-am tags uninstall uninstall-am \
-	uninstall-binPROGRAMS uninstall-libLTLIBRARIES \
+.PHONY: CTAGS GTAGS all all-am check check-am clean clean-generic \
+	clean-libLTLIBRARIES clean-libtool ctags distclean \
+	distclean-compile distclean-generic distclean-libtool \
+	distclean-tags distdir dvi dvi-am html html-am info info-am \
+	install install-am install-data install-data-am install-dvi \
+	install-dvi-am install-exec install-exec-am install-html \
+	install-html-am install-info install-info-am \
+	install-libLTLIBRARIES install-lib_menu_cache_includeHEADERS \
+	install-man install-pdf install-pdf-am install-pkgconfigDATA \
+	install-ps install-ps-am install-strip installcheck \
+	installcheck-am installdirs maintainer-clean \
+	maintainer-clean-generic mostlyclean mostlyclean-compile \
+	mostlyclean-generic mostlyclean-libtool pdf pdf-am ps ps-am \
+	tags uninstall uninstall-am uninstall-libLTLIBRARIES \
 	uninstall-lib_menu_cache_includeHEADERS \
 	uninstall-pkgconfigDATA
 

libmenu-cache/menu-cache.c
@@ -26,6 +26,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/fcntl.h>
+#include <errno.h>
 
 #include "menu-cache.h"
 
@@ -64,12 +68,17 @@ struct _MenuCache
 {
 	guint n_ref;
 	MenuCacheDir* root_dir;
+	char* menu_file_path;
+	char* cache_file_path;
 	char** all_used_dirs;
 	int n_all_used_dirs;
-	char* menu_file_path;
 	time_t mtime;
 	time_t time;
+	GSList* notifiers;
 };
+
+static int server_fd = -1;
+static GIOChannel* server_ch = NULL;
 
 void menu_cache_init()
 {
@@ -284,6 +293,7 @@ MenuCache* menu_cache_new( const char* cache_file, char** include, char** exclud
 	cache = g_slice_new0( MenuCache );
 	cache->mtime = st.st_mtime;
 
+	cache->cache_file_path = g_strdup( cache_file );
 	cache->menu_file_path = g_strdup( strtok(line, "\n") );
 
 	/* get all used dirs */
@@ -311,6 +321,8 @@ void menu_cache_unref(MenuCache* cache)
 	{
 		if( G_LIKELY(cache->root_dir) )
 			menu_cache_item_unref( cache->root_dir );
+		g_free( cache->cache_file_path );
+		g_free( cache->menu_file_path );
 		g_strfreev( cache->all_used_dirs );
 		g_slice_free( MenuCache, cache );
 	}
@@ -326,6 +338,64 @@ MenuCacheItem* menu_cache_item_ref(MenuCacheItem* item)
 {
 	g_atomic_int_inc( &item->n_ref );
 	return item;
+}
+
+typedef struct _CacheReloadNotifier
+{
+	GFunc func;
+	gpointer user_data;
+}CacheReloadNotifier;
+
+gpointer menu_cache_add_reload_notify(MenuCache* cache, GFunc func, gpointer user_data)
+{
+	GSList* l = g_slist_alloc();
+	CacheReloadNotifier* n = g_slice_new(CacheReloadNotifier);
+	n->func = func;
+	n->user_data = user_data;
+	l->data = n;
+	cache->notifiers = g_slist_concat( cache->notifiers, l );
+	return l;
+}
+
+void menu_cache_remove_reload_notify(MenuCache* cache, gpointer notify_id)
+{
+	g_slice_free( CacheReloadNotifier, ((GSList*)notify_id)->data );
+	cache->notifiers = g_list_delete_link( cache->notifiers, (GSList*)notify_id );
+}
+
+gboolean menu_cache_reload( MenuCache* cache )
+{
+#if 0
+	FILE* f = fopen( cache_file, "r" );
+	if( ! f )
+		return FALSE;
+
+	if( fstat( fileno( f ), &st ) == -1 )
+	{
+		fclose( f );
+		return FALSE;
+	}
+
+	if( ! fgets( line, G_N_ELEMENTS(line) ,f ) )
+		return FALSE;
+
+	cache->mtime = st.st_mtime;
+
+	cache->cache_file_path = g_strdup( cache_file );
+	cache->menu_file_path = g_strdup( strtok(line, "\n") );
+
+	/* get all used dirs */
+	if( ! read_all_used_dirs( f, &cache->n_all_used_dirs, &cache->all_used_dirs ) )
+	{
+		g_slice_free( MenuCache, cache );
+		return FALSE;
+	}
+	cache->n_ref = 1;
+	cache->root_dir = (MenuCacheDir*)read_item( f, cache );
+	cache->time = time(NULL);	/* save current time */
+	fclose( f );
+#endif
+	return TRUE;
 }
 
 void menu_cache_item_unref(MenuCacheItem* item)
@@ -532,6 +602,19 @@ gboolean menu_cache_is_updated( MenuCache* cache )
 	struct stat st;
 	int i;
 
+	/* on-disk cache file is newer than our in-memory data? */
+	if( stat( cache->cache_file_path, &st ) == -1 )
+		return FALSE;
+	if( st.st_mtime > cache->mtime )
+		return FALSE;
+
+	/* source *.menu file is newer? */
+	if( stat( cache->menu_file_path, &st ) == -1 )
+		return FALSE;
+	if( st.st_mtime > cache->mtime )
+		return FALSE;
+
+	/* anyone of the involved dirs is newer? */
 	for( i = 0; i < cache->n_all_used_dirs; ++i )
 	{
 		if( stat( cache->all_used_dirs[i], &st ) == -1 )
@@ -543,3 +626,222 @@ gboolean menu_cache_is_updated( MenuCache* cache )
 	return TRUE;
 }
 
+static void get_socket_name( char* buf, int len )
+{
+    char* dpy = g_getenv("DISPLAY");
+    g_snprintf( buf, len, "/tmp/.menu-cached-%s-%s", dpy, g_get_user_name() );
+}
+
+#define MAX_RETRIES 25
+
+static gboolean fork_server()
+{
+    const char *server_path = g_find_program_in_path("menu-cached");
+    int ret, pid, status;
+
+    if (!server_path)
+    {
+        g_print("failed to find menu-cached\n");
+    }
+
+    /* Become a daemon */
+    pid = fork();
+    if (pid == 0)
+    {
+        int fd;
+        long open_max;
+        long i;
+
+        /* don't hold open fd opened from the client of the library */
+        open_max = sysconf (_SC_OPEN_MAX);
+        for (i = 0; i < open_max; i++)
+            fcntl (i, F_SETFD, FD_CLOEXEC);
+
+        /* /dev/null for stdin, stdout, stderr */
+        fd = open ("/dev/null", O_RDONLY);
+        if (fd != -1)
+        {
+            dup2 (fd, 0);
+            close (fd);
+        }
+        fd = open ("/dev/null", O_WRONLY);
+        if (fd != -1)
+        {
+            dup2 (fd, 1);
+            dup2 (fd, 2);
+            close (fd);
+        }
+        setsid();
+        if (fork() == 0)
+        {
+            execl( server_path, server_path, NULL);
+            g_print("failed to exec %s\n", server_path);
+        }
+        _exit(0);
+    }
+
+    /*
+     * do a waitpid on the intermediate process to avoid zombies.
+     */
+retry_wait:
+    ret = waitpid(pid, &status, 0);
+    if (ret < 0) {
+        if (errno == EINTR)
+            goto retry_wait;
+    }
+    return TRUE;
+}
+
+static gboolean on_server_io(GIOChannel* ch, GIOCondition cond, gpointer user_data)
+{
+    GIOStatus st;
+    char* line;
+    gsize len;
+    g_debug("server IO: %d", cond);
+retry:
+    st = g_io_channel_read_line( ch, &line, &len, NULL, NULL );
+    if( st == G_IO_STATUS_AGAIN )
+        goto retry;
+    if ( st != G_IO_STATUS_NORMAL || len < 4 )
+    {
+        g_io_channel_unref(ch);
+        g_debug("server IO error!!");
+        return FALSE;
+    }
+
+    if( memcmp( line, "REL:", 4 ) ) /* reload */
+    {
+        char* menu_cache_id = line + 4;
+
+    }
+
+    g_free( line );
+    return TRUE;
+}
+
+static gboolean connect_server()
+{
+    int fd;
+    struct sockaddr_un addr;
+    int retries = 0;
+
+    if( server_fd != -1 )
+        return TRUE;
+
+retry:
+    fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0)
+    {
+        g_print("Failed to create socket\n");
+        return FALSE;
+    }
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+
+    get_socket_name( addr.sun_path, sizeof( addr.sun_path ) );
+
+    if( connect(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+    {
+        if(retries == 0)
+        {
+            close(fd);
+            fork_server();
+            ++retries;
+            goto retry;
+        }
+        if (retries < MAX_RETRIES)
+        {
+            close(fd);
+            usleep(5000);
+            ++retries;
+            goto retry;
+        }
+        g_print("Unable to connect\n");
+        close(fd);
+        return FALSE;
+    }
+    server_fd = fd;
+    server_ch = g_io_channel_unix_new(fd);
+    g_io_channel_set_close_on_unref(server_ch, TRUE);
+    g_io_add_watch(server_ch, G_IO_IN|G_IO_PRI|G_IO_ERR|G_IO_HUP, on_server_io, NULL);
+    return TRUE;
+}
+
+static char* register_menu_to_server( const char* menu_name )
+{
+    const gchar * const * langs = g_get_language_names();
+    const char* xdg_cfg = g_getenv("XDG_CONFIG_DIRS");
+    const char* xdg_prefix = g_getenv("XDG_MENU_PREFIX");
+    const char* xdg_data = g_getenv("XDG_DATA_DIRS");
+    const char* xdg_cfg_home = g_getenv("XDG_CONFIG_HOME");
+    const char* xdg_data_home = g_getenv("XDG_DATA_HOME");
+    char* buf;
+    char md5[36];
+    int len = 0, r;
+
+    if( !xdg_cfg )
+        xdg_cfg = "";
+    if( ! xdg_prefix )
+        xdg_prefix = "";
+    if( ! xdg_data )
+        xdg_data = "";
+    if( ! xdg_cfg_home )
+        xdg_cfg_home = "";
+    if( ! xdg_data_home )
+        xdg_data_home = "";
+
+    /* get rid of the encoding part of locale name. */
+    while( strchr(langs[0], '.') )
+        ++langs;
+
+    buf = g_strdup_printf( "REG:%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+                            menu_name,
+                            *langs,
+                            xdg_cfg,
+                            xdg_prefix,
+                            xdg_data,
+                            xdg_cfg_home,
+                            xdg_data_home );
+    write( server_fd, buf, strlen(buf) );
+
+    while( (r=read(server_fd, md5 + len, 32 - len)) > 0 && len < 32 )
+        len += r;
+    md5[32] = '\0';
+
+    g_debug("md5: %s", md5);
+
+    if( r == -1 )
+    {
+        g_debug("server error!");
+        return NULL;
+    }
+
+    g_free( buf );
+
+    return len == 32 ? g_build_filename( g_get_user_cache_dir(), "menus", md5, NULL ) : NULL;
+}
+
+static void unregister_menu_from_server( const char* menu_cache_name )
+{
+    char* buf = g_strdup_printf("UNR:%s\n", menu_cache_name );
+    write( server_fd, buf, strlen(buf) );
+    g_free( buf );
+}
+
+MenuCache* menu_cache_lookup( const char* menu_name )
+{
+    MenuCache* cache;
+    char* file_name;
+    /* FIXME: lookup in a hash table for already loaded menus */
+
+    if( !connect_server() )
+    {
+        g_print("unable to connect to menu-cached.\n");
+        return NULL;
+    }
+    file_name = register_menu_to_server( menu_name );
+    g_debug("file_name = %s", file_name);
+    cache = menu_cache_new( file_name, NULL, NULL );
+    g_free( file_name );
+    return cache;
+}

libmenu-cache/menu-cache.h
@@ -49,13 +49,21 @@ enum _MenuCacheType
 
 void menu_cache_init();
 
+MenuCache* menu_cache_lookup( const char* menu_name );
+
+/* Don't call this API directly. Use menu_cache_lookup instead. */
 MenuCache* menu_cache_new( const char* cache_file, char** include, char** exclude );
 
 MenuCache* menu_cache_ref(MenuCache* cache);
 void menu_cache_unref(MenuCache* cache);
 
+gboolean menu_cache_reload( MenuCache* cache );
+
 MenuCacheDir* menu_cache_get_root_dir( MenuCache* cache );
 MenuCacheDir* menu_cache_get_dir_from_path( MenuCache* cache, const char* path );
+
+gpointer menu_cache_add_reload_notify(MenuCache* cache, GFunc func, gpointer user_data);
+void menu_cache_remove_reload_notify(MenuCache* cache, gpointer notify_id);
 
 gboolean menu_cache_file_is_updated( const char* menu_file );
 gboolean menu_cache_is_updated( MenuCache* cache );

commit 0335cce074ca8d131d02897a5813e81f2c6e764c

Open listening socket in the child, after closing all file descriptors.
Otherwise we step into closing it by freopen() in case if stdin was closed before.
This should finally fix the issue with accept() failure introduced by commit
c8794da0cf5a194b5eaadf020f02a5cab89f54fc very early which did dup2() on stdin
without any check if it's not assigned to the listening socket.

menu-cache-daemon/menu-cached.c
@@ -46,6 +46,7 @@
 #include <errno.h>
 #include <signal.h>
 #include <utime.h>
+#include <sys/wait.h>
 
 #ifdef G_ENABLE_DEBUG
 #define DEBUG(...)  g_debug(__VA_ARGS__)
@@ -857,9 +858,45 @@ int main(int argc, char** argv)
     struct sockaddr_un addr;
 #ifndef DISABLE_DAEMONIZE
     pid_t pid;
+    int status;
 
     long open_max;
     long i;
+#endif
+
+#ifndef DISABLE_DAEMONIZE
+    /* Become a daemon */
+    if ((pid = fork()) < 0) {
+        g_error("can't fork");
+        return 2;
+    }
+    else if (pid != 0) {
+        /* wait for result of child */
+        while ((i = waitpid(pid, &status, 0)) < 0 && errno == EINTR);
+        if (i < 0 || !WIFEXITED(status)) /* system error or child crashed */
+            return 127;
+        /* exit parent */
+        return WEXITSTATUS(status);
+    }
+
+    /* reset session to forget about parent process completely */
+    setsid();
+
+    /* change working directory to root, so previous working directory
+     * can be unmounted */
+    if (chdir("/") < 0) {
+        g_error("can't change directory to /");
+    }
+
+    /* don't hold open fd opened besides server socket and std{in,out,err} */
+    open_max = sysconf (_SC_OPEN_MAX);
+    for (i = 3; i < open_max; i++)
+        close (i);
+
+    /* /dev/null for stdin, stdout, stderr */
+    if (freopen("/dev/null", "r", stdin)) i = i;
+    if (freopen("/dev/null", "w", stdout)) i = i;
+    if (freopen("/dev/null", "w", stderr)) i = i;
 #endif
 
     memset(&addr, 0, sizeof(addr));
@@ -882,36 +919,20 @@ int main(int argc, char** argv)
         return 1;
 
 #ifndef DISABLE_DAEMONIZE
-    /* Become a daemon */
+    /* Second fork to let parent get a result */
     if ((pid = fork()) < 0) {
         g_error("can't fork");
+        close(server_fd);
+        unlink(socket_file);
+        return 2;
+    } else if (pid != 0) {
+        /* wait for result of child */
+        if (waitpid(pid, &status, 0) < 0 || !WIFEXITED(status))
+            return 127;
+        /* exit child */
+        return 0;
     }
-    else if (pid != 0) {
-        /* exit parent */
-        exit(0);
-    }
-
-    /* reset session to forget about parent process completely */
-    setsid();
-
-    /* change working directory to root, so previous working directory
-     * can be unmounted */
-    if (chdir("/") < 0) {
-        g_error("can't change directory to /");
-    }
-
-    /* don't hold open fd opened besides server socket and std{in,out,err} */
-    open_max = sysconf (_SC_OPEN_MAX);
-    for (i = 3; i < open_max; i++)
-    {
-        if (i != server_fd)
-            close (i);
-    }
-
-    /* /dev/null for stdin, stdout, stderr */
-    if (freopen("/dev/null", "r", stdin)) i = i;
-    if (freopen("/dev/null", "w", stdout)) i = i;
-    if (freopen("/dev/null", "w", stderr)) i = i;
+    /* We are in grandchild now, daemonized */
 #endif
 
     signal(SIGHUP, terminate);